a8edd8019801dc92bb75ae1987cd7a60de330fcd
Feminask <fmnzrasheed@gmail.com>
2024-06-26 21:53:55 +0530

    connect to the database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(
+    ()=>{console.log('Mongodb is connected');}
+).catch(err=>{
+    console.log(err);
+})
 
 const app =express();
-const PORT=3000
+const PORT=8007
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}... `);
 })
