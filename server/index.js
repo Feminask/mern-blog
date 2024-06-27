6764ebbcbf042f03d618599a1416e468b151b507
Feminask <fmnzrasheed@gmail.com>
2024-06-27 21:27:19 +0530

    create signup api route

--- server/index.js
+++ server/index.js
@@ -1,21 +1,27 @@
-import express from 'express'
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(
-    ()=>{console.log('Mongodb is connected');}
-).catch(err=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Mongodb is connected");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-const app =express();
-const PORT=9000
-app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}... `);
-})
+const app = express();
+app.use(express.json());
 
-app.use('/api/user',userRoutes)
+const PORT = 9000;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}... `);
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
---
+++ server/routes/auth.route.js
@@ -0,0 +1,10 @@
+// server/routes/auth.route.js
+
+import express from 'express';
+import { signup } from '../controllers/auth.controller.js'; // Ensure the path is correct and includes .js
+
+const router = express.Router();
+
+router.post('/signup', signup);
+
+export default router;
