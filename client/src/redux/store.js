f6efc21e6c9481e6ff4000e4f82c966201612859
Feminask <fmnzrasheed@gmail.com>
2024-06-28 11:54:36 +0530

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,57 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore,combineReducers } from "@reduxjs/toolkit";
+// import userReducer from "./user/userSlice";
+// import storage from "redux-persist/lib/storage";
+// import { version } from "react";
+// import persistReducer from "redux-persist/es/persistReducer";
+// import persistStore from "redux-persist/es/persistStore";
+
+
+// const rootReducer = combineReducers({
+//     user:userReducer
+// })
+
+// const persistConfig={
+//     key:'root',
+//     storage,
+//     version:1,
+
+// }
+
+// const persistedReducer= persistReducer(persistConfig,rootReducer)
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware:(getDefaultMiddleware)=>{
+//     getDefaultMiddleware({serializableCheck:false})
+//   }
+// });
+// export const persistor = persistStore(store)
+
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+import storage from "redux-persist/lib/storage";
+import { version } from "react";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({
+    user: userReducer
 });
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
+
+export const persistor = persistStore(store);
