a4d5652dcac669c092b78632036746c652ee091d
Feminask <fmnzrasheed@gmail.com>
2024-06-30 23:01:29 +0530

    complt post page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import PrivateRoute from './components/PrivateRoute'
 import CreatePost from './pages/CreatePost'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 import UpdatePost from './pages/UpdatePost'
+import PostPage from './pages/PostPage'
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
       </Route>
 
       <Route path='/projects' element={<Projects></Projects>}></Route>
+      <Route path='/post/:postSlug' element={<PostPage></PostPage>}></Route>
 
      </Routes>
      <FooterCom></FooterCom>
